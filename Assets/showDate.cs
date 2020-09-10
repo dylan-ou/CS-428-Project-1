4145b4af8d33b0ee4e5e360c2c05a400c95e5843
Dylan Ou <dylan.ou.5597@gmail.com>
2020-09-10 02:20:18 -0500

    Added files

---
+++ Assets/showDate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+
+public class showDate : MonoBehaviour
+{
+    public TextMeshPro text;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        text.SetText(System.DateTime.Now.ToString("M/d/yy"));
+    }
+}
---
+++ Assets/showTime.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class showTime : MonoBehaviour
+{
+    public TextMeshPro text;
+
+    
+    void Update()
+    {
+        text.SetText(System.DateTime.Now.ToString("hh:mm tt"));
+    }
+}
