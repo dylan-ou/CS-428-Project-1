3440a8d2563f26a905ee8efe47d331df137c5fc4
Dylan Ou <dylan.ou.5597@gmail.com>
2020-09-18 17:22:43 -0500

    Weather widget now supports all weather type

--- Assets/My Assets/weatherScript.cs
+++ Assets/My Assets/weatherScript.cs
@@ -13,14 +13,16 @@ public class weatherScript : MonoBehaviour
     public TextMeshPro text;
     public GameObject[] weatherObjects = new GameObject[9];
 
-    Regex condition = new Regex("(clear sky)|(few clouds)|(scattered clouds)|(broken clouds)|(shower rain)|(rain)|(thunderstorm)|(snow)|(mist)");
-    Regex condition2 = new Regex("\"id\":(800|80[1-4]|5[0-3][0-4]|2[0-3][0-2]|6[0-2][0-6]|7[0-8][1-2])");
+    //Regex condition = new Regex("(clear sky)|(few clouds)|(scattered clouds)|(broken clouds)|(shower rain)|(rain)|(thunderstorm)|(snow)|(mist)");
+    Regex condition2 = new Regex("\"id\":[0-9]{3}");
     string currentCondition = "";
+    string conditionID = "";
     string[] listOfConditions = { "clear sky", "few clouds", "scattered clouds", "broken clouds", "shower rain", "rain", "thunderstorm", "snow", "mist" };
 
     string webText = "";
     Match weatherMatch;
     int currentCase = -1;
+    int idNum;
 
     public
     // Start is called before the first frame update
@@ -38,14 +40,85 @@ public class weatherScript : MonoBehaviour
     {
         if(webText == "") {
             webText = api.GetWebText();
-            weatherMatch = condition.Match(webText);
-            currentCondition = weatherMatch.Groups[0].Value;
+            weatherMatch = condition2.Match(webText);
+            conditionID = weatherMatch.Groups[0].Value;
+            idNum = Int32.Parse(conditionID.Substring(5));
         }
 
         Debug.Log("RN it's " + currentCondition);
 
         if (webText != "" && currentCase == -1) 
         {
+            switch (idNum)
+            {
+                case 800:
+                    currentCondition = "clear sky";
+                    break;
+                case 801:
+                    currentCondition = "few clouds";
+                    break;
+                case 802:
+                    currentCondition = "scattered clouds";
+                    break;
+                case 803:
+                case 804:
+                    currentCondition = "broken clouds";
+                    break;
+                case 520:
+                case 521:
+                case 522:
+                case 531:
+                    currentCondition = "shower rain";
+                    break;
+                case 500:
+                case 501:
+                case 502:
+                case 503:
+                case 504:
+                case 511:
+                    currentCondition = "rain";
+                    break;
+                case 200:
+                case 201:
+                case 202:
+                case 210:
+                case 211:
+                case 212:
+                case 221:
+                case 230:
+                case 231:
+                case 232:
+                    currentCondition = "thunderstorm";
+                    break;
+                case 600:
+                case 601:
+                case 602:
+                case 611:
+                case 612:
+                case 613:
+                case 615:
+                case 616:
+                case 620:
+                case 621:
+                case 622:
+                    currentCondition = "snow";
+                    break;
+                case 701:
+                case 711:
+                case 721:
+                case 731:
+                case 741:
+                case 751:
+                case 761:
+                case 762:
+                case 771:
+                case 781:
+                    currentCondition = "mist";
+                    break;
+                default:
+                    currentCase = 0;
+                    break;
+            }
             currentCase = Array.IndexOf(listOfConditions, currentCondition);
         } 
         else if(webText != "" && currentCase != -1)
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CS-428-Project-1
+
+Sound effects:
+Rain Inside House: http://soundbible.com/2065-Rain-Inside-House.html
+Thunder Strike 2: http://soundbible.com/2016-Thunder-Strike-2.html
+Rainforest: http://soundbible.com/1818-Rainforest-Ambience.html
+Light Rain: http://soundbible.com/1192-Light-Rain-And-Crickets.html
